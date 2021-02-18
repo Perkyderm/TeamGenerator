8b78e224068a706ee9d634201087d1c84c234b65
Perkyderm <ncgoedecke@gmail.com>
2021-02-18 07:46:01 -0700

    temp

---
+++ index.js
@@ -0,0 +1,25 @@
+const inquireer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const Manager = require("./JS/Manager");
+const Employee = require("./JS/Employee");
+const Intern = require("./JS/Intern");
+const Engineer = require("./JS/Engineer");
+
+const teamMember = [];
+
+const id = [];
+
+function menu() {
+  function createManager() {}
+
+  function addTeamMemeber() {}
+
+  function addEngineer() {}
+
+  function addIntern() {}
+
+  function buildHTML() {}
+}
+
+menu();
